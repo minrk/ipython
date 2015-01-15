161a575aa211c406198b6f337651128ca56afc92
Min RK <benjaminrk@gmail.com>
2015-01-15 15:46:08 -0800

    wtf, debug

--- IPython/html/terminal/api_handlers.py
+++ IPython/html/terminal/api_handlers.py
@@ -1,5 +1,5 @@
 import json
-from tornado import web
+from tornado import web, gen, ioloop
 from ..base.handlers import IPythonHandler, json_errors
 from ..utils import url_path_join
 
@@ -33,10 +33,15 @@ class TerminalHandler(IPythonHandler):
 
     @web.authenticated
     @json_errors
+    @gen.coroutine
     def delete(self, name):
         tm = self.terminal_manager
         if name in tm.terminals:
             tm.kill(name)
+            loop = ioloop.IOLoop.current()
+            while name in tm.terminals:
+                self.log.info("Waiting for %s to die", name)
+                yield gen.Task(loop.add_timeout, loop.time() + 0.5)
             # XXX: Should this wait for terminal to finish before returning?
             self.set_status(204)
             self.finish()
